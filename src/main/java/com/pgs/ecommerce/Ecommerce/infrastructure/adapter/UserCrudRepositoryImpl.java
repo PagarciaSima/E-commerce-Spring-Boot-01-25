0fe099944d0719cca031f4a20f7d0dc11d6deb0d
pagarciaSim <pablo.garciasimavilla@gmail.com>
2025-01-21 15:22:51 +0100

    UserRepoImpl

--- src/main/java/com/pgs/ecommerce/Ecommerce/infrastructure/adapter/UserCrudRepositoryImpl.java
+++ src/main/java/com/pgs/ecommerce/Ecommerce/infrastructure/adapter/UserCrudRepositoryImpl.java
@@ -2,6 +2,7 @@ package com.pgs.ecommerce.Ecommerce.infrastructure.adapter;
 
 import com.pgs.ecommerce.Ecommerce.domain.model.User;
 import com.pgs.ecommerce.Ecommerce.domain.port.IUserRepository;
+import com.pgs.ecommerce.Ecommerce.infrastructure.mapper.UserMapper;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Repository;
 
@@ -10,19 +11,19 @@ import org.springframework.stereotype.Repository;
 public class UserCrudRepositoryImpl implements IUserRepository {
 
     private final IUserCrudRepository iUserCrudRepository;
+    private final UserMapper userMapper;
 
     @Override
     public User save(User user) {
-        return null;
+        return userMapper.toUser(iUserCrudRepository.save(userMapper.toUserEntity(user)));
     }
 
     @Override
     public User findById(Integer id) {
-        return null;
+        return userMapper.toUser(iUserCrudRepository.findById(id).get());
     }
 
     @Override
     public User findByEmail(String email) {
-        return null;
     }
 }
