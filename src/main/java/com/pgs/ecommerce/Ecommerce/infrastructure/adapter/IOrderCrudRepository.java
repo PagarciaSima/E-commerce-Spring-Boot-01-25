89bdaf568ca84a7a322d44f1ff13a5af12df3e99
pagarciaSim <pablo.garciasimavilla@gmail.com>
2025-01-22 14:35:06 +0100

    Order repo

---
+++ src/main/java/com/pgs/ecommerce/Ecommerce/infrastructure/adapter/IOrderCrudRepository.java
@@ -0,0 +1,19 @@
+package com.pgs.ecommerce.Ecommerce.infrastructure.adapter;
+
+import com.pgs.ecommerce.Ecommerce.domain.model.OrderState;
+import com.pgs.ecommerce.Ecommerce.infrastructure.entity.OrderEntity;
+import com.pgs.ecommerce.Ecommerce.infrastructure.entity.UserEntity;
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+public interface IOrderCrudRepository extends CrudRepository<OrderEntity, Integer> {
+
+    @Transactional
+    @Modifying
+    @Query("UPDATE OrderEntity o SET o.state = :state WHERE o.id = :id")
+    void updateStateById(Integer id, OrderState state);
+
+    Iterable<OrderEntity> findByUserEntity(UserEntity userEntity);
+}
