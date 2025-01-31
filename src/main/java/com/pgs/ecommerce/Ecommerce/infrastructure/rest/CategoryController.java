54748197efd1bdf94c4730432c71af2cbdf0c4a8
pagarciaSim <pablo.garciasimavilla@gmail.com>
2025-01-31 06:27:43 +0100

    Category controller test

--- src/main/java/com/pgs/ecommerce/Ecommerce/infrastructure/rest/CategoryController.java
+++ src/main/java/com/pgs/ecommerce/Ecommerce/infrastructure/rest/CategoryController.java
@@ -1,52 +1,73 @@
 package com.pgs.ecommerce.Ecommerce.infrastructure.rest;
 
-import com.pgs.ecommerce.Ecommerce.application.CategoryService;
-import com.pgs.ecommerce.Ecommerce.domain.model.Category;
-import lombok.AllArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-
-import java.util.NoSuchElementException;
+import java.util.Collection;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.pgs.ecommerce.Ecommerce.application.CategoryService;
+import com.pgs.ecommerce.Ecommerce.domain.model.Category;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @CrossOrigin(origins = "http://localhost:4200/")
 @RestController
 @RequestMapping("api/v1/admin/categories")
 @AllArgsConstructor
 @Slf4j
+
 // http://localhost:8085/api/v1/admin/categories
 public class CategoryController {
 
     private final CategoryService categoryService;
-
+    
     @PostMapping
     public ResponseEntity<Category> save(@RequestBody Category category) {
-        return new ResponseEntity<>(this.categoryService.save(category), HttpStatus.CREATED);
+        log.info("Saving new category: {}", category);
+        Category savedCategory = this.categoryService.save(category);
+        log.info("Category saved successfully with id: {}", savedCategory.getId());
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedCategory);
     }
     
     @PutMapping("/{id}")
-    public ResponseEntity<?> update(@PathVariable Integer id, @RequestBody Category category) {
-        try {
-            return ResponseEntity.ok(this.categoryService.update(id, category));
-        } catch (NoSuchElementException e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Category not found with id: " + id);
-        }
+    public ResponseEntity<Category> update(@PathVariable Integer id, @RequestBody Category category) {
+        log.info("Updating category with id: {}", id);
+        Category updatedCategory = this.categoryService.update(id, category);
+        log.info("Category updated successfully: {}", updatedCategory);
+        return ResponseEntity.ok(updatedCategory);
     }
+    
     @GetMapping
-    public ResponseEntity<Iterable<Category>> findAll () {
-        return ResponseEntity.ok(this.categoryService.findAll());
+    public ResponseEntity<Iterable<Category>> findAll() {
+        log.info("Fetching all categories...");
+        Iterable<Category> categories = this.categoryService.findAll();
+        log.info("Fetched {} categories", ((Collection<?>) categories).size());
+        return ResponseEntity.ok(categories);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Category> findById (@PathVariable Integer id) {
-        return ResponseEntity.ok(this.categoryService.findById(id));
+    public ResponseEntity<Category> findById(@PathVariable Integer id) {
+        log.info("Fetching category with id: {}", id);
+        Category category = this.categoryService.findById(id);
+        log.info("Category found: {}", category);
+        return ResponseEntity.ok(category);
     }
-
+    
     @DeleteMapping("/{id}")
-    public ResponseEntity<Category> deleteById (@PathVariable Integer id) {
+    public ResponseEntity<Void> deleteById(@PathVariable Integer id) {
+        log.warn("Deleting category with id: {}", id);
         this.categoryService.deleteById(id);
-        return ResponseEntity.ok().build();
+        log.warn("Category deleted successfully");
+        return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/com/pgs/ecommerce/Ecommerce/infrastructure/rest/UserController.java
+++ src/main/java/com/pgs/ecommerce/Ecommerce/infrastructure/rest/UserController.java
@@ -9,10 +9,10 @@ import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController
-// http://localhost:8085/api/v1/users
 @RequestMapping("/api/v1/users")
 @AllArgsConstructor
 @Slf4j
+//http://localhost:8085/api/v1/users
 
 public class UserController {
 
