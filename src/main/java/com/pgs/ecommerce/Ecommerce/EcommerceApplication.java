9c7b3b3fb4ab7c1667732b6922f7da1923c8c66b
pagarciaSim <pablo.garciasimavilla@gmail.com>
2025-01-31 17:04:49 +0100

    configuring dotEnv & paypal config

--- src/main/java/com/pgs/ecommerce/Ecommerce/EcommerceApplication.java
+++ src/main/java/com/pgs/ecommerce/Ecommerce/EcommerceApplication.java
@@ -3,10 +3,19 @@ package com.pgs.ecommerce.Ecommerce;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class EcommerceApplication {
 
 	public static void main(String[] args) {
+		 Dotenv dotenv = Dotenv.configure().load();
+
+	        // Acceder a las variables de entorno
+	        String paypalClientId = dotenv.get("PAYPAL_CLIENT_ID");
+	        String paypalSecret = dotenv.get("PAYPAL_SECRET");
+	        System.out.println("PayPal Client ID: " + paypalClientId);
+
 		SpringApplication.run(EcommerceApplication.class, args);
 	}
 
