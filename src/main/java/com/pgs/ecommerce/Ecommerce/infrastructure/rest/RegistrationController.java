35d10ea488b30a0859ffbcd008bb07a06bd886af
pagarciaSim <pablo.garciasimavilla@gmail.com>
2025-02-03 14:56:31 +0100

    Implementing endpoint to register users

---
+++ src/main/java/com/pgs/ecommerce/Ecommerce/infrastructure/rest/RegistrationController.java
@@ -0,0 +1,35 @@
+package com.pgs.ecommerce.Ecommerce.infrastructure.rest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.pgs.ecommerce.Ecommerce.application.RegistrationService;
+import com.pgs.ecommerce.Ecommerce.domain.model.User;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@CrossOrigin(origins = "http://localhost:4200")
+@RestController
+@RequestMapping("api/v1/security")
+@AllArgsConstructor
+@Slf4j
+public class RegistrationController {
+
+	private final RegistrationService registrationService;
+	
+	@PostMapping("/register")
+	public ResponseEntity<User> register(@RequestBody User user) {
+	    log.info("Request to register new user with email: {}", user.getEmail());
+	    User registeredUser = registrationService.register(user);
+	    log.info("User registered successfully with email: {}", registeredUser.getEmail());
+	    return new ResponseEntity<>(registeredUser, HttpStatus.CREATED);
+	}
+
+	
+}
