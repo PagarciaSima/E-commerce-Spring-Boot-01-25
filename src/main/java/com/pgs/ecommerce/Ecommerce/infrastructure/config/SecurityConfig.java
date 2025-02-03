d60a6abfb728f8c6a8fdb0ed126375937f113445
pagarciaSim <pablo.garciasimavilla@gmail.com>
2025-02-03 15:22:35 +0100

    Configuring security filter chain

---
+++ src/main/java/com/pgs/ecommerce/Ecommerce/infrastructure/config/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.pgs.ecommerce.Ecommerce.infrastructure.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+	@Bean
+	SecurityFilterChain filterChain (HttpSecurity httpSecurity) throws Exception {
+		httpSecurity.csrf(csrf -> csrf.disable()).authorizeHttpRequests(
+				auth -> auth.requestMatchers("api/v1/security/**").permitAll()
+				.anyRequest().authenticated()
+		);
+		return httpSecurity.build();
+	}
+}
