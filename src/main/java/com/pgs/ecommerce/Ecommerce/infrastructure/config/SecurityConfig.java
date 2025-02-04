43574a62c02928c0808d7a4f9f0bfb965a2efadd
pagarciaSim <pablo.garciasimavilla@gmail.com>
2025-02-04 08:19:35 +0100

    Implemting auth bean

--- src/main/java/com/pgs/ecommerce/Ecommerce/infrastructure/config/SecurityConfig.java
+++ src/main/java/com/pgs/ecommerce/Ecommerce/infrastructure/config/SecurityConfig.java
@@ -2,6 +2,8 @@ package com.pgs.ecommerce.Ecommerce.infrastructure.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -23,5 +25,11 @@ public class SecurityConfig {
 	@Bean
 	BCryptPasswordEncoder passwordEncoder () {
 		return new BCryptPasswordEncoder();
+	}
+	
+	// Delegates authentication from filter chain to Custom User Detail Service
+	@Bean
+	AuthenticationManager authenticationManager (AuthenticationConfiguration authenticationConfiguration) throws Exception {
+		return authenticationConfiguration.getAuthenticationManager();
 	}
 }
